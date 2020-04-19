0083066867d634e5b47ee2ebab7b60c35466188e
adityapandit17 <adityapandit38@gmail.com>
2020-04-20 03:45:45 +0530

    Fixed session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,11 +4,11 @@ class Users::SessionsController < Devise::SessionsController
   before_action :authorize_request, except: :create
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user&.valid_password?(params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'), id: @user.id, email: @user.email }, status: :ok
+    user = User.find_by_email(params[:email])
+    if user&.valid_password?(params[:password])
+      token = JsonWebToken.encode(user_id: user.id)
+      time = 1.day.from_now
+      render json: { token: token, exp: time, id: user.id, email: user.email }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
